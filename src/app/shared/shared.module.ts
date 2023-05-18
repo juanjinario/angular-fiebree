452e61b17b2523e6fbe784980e24bc80251e76ec
juanjinario <juanjco2@gmail.com>
2023-05-18 22:44:42 +0200

    feat: add material imports module & mat-toolbar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { MaterialModule } from './modules/material.module';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [HeaderComponent, MaterialModule],
+})
+export class SharedModule {}
