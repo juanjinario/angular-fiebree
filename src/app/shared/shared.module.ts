67d803576b6a1399932c5d878b832c2e7b3f4b92
juanjinario <juanjco2@gmail.com>
2023-05-18 22:52:09 +0200

    feat: add mat-drawer and navigation layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { MaterialModule } from './modules/material.module';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NavigationComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [HeaderComponent, MaterialModule],
+  exports: [HeaderComponent, MaterialModule, NavigationComponent],
 })
 export class SharedModule {}
