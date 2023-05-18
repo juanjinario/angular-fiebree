34c93dfcb15eb0a2a3e17722c3912d8ddf0adc7e
juanjinario <juanjco2@gmail.com>
2023-05-18 23:36:51 +0200

    feat: add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { MaterialModule } from './modules/material.module';
 import { NavigationComponent } from './layout/navigation/navigation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, NavigationComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [HeaderComponent, MaterialModule, NavigationComponent],
 })
 export class SharedModule {}
